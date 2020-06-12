c78bfdb23e63ef1bea246e420b8dd66bd1b33563
Sera Aboagye <saboagye@flexera.com>
2020-06-11 22:24:10 -0500

    mived files

--- basics/ec2.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "web" {
-  ami           = "$ami-number"
-  instance_type = "t2.micro"
-  key_name = "${aws_key_pair.deployer.key_name}"
-
-  tags = {
-    Name = "HelloWorld"
-  }
-}
