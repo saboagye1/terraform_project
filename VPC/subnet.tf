fd6ca26733463bc112757fb462186ee5612dbaff
Sera Aboagye <saboagye@flexera.com>
2020-06-16 14:53:33 -0500

    ips updated

--- VPC/subnet.tf
+++ VPC/subnet.tf
@@ -21,15 +21,15 @@ resource "aws_subnet" "dev3" {
 
 resource "aws_subnet" "dev_private1" {
   vpc_id = "${aws_vpc.dev.id}"
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.101.0/24"
 }
 
 resource "aws_subnet" "dev_private2" {
   vpc_id = "${aws_vpc.dev.id}"
-  cidr_block = "10.0.2.0/24"
+  cidr_block = "10.0.102.0/24"
 }
 
 resource "aws_subnet" "dev_private3" {
   vpc_id = "${aws_vpc.dev.id}"
-  cidr_block = "10.0.3.0/24"
+  cidr_block = "10.0.103.0/24"
 }
