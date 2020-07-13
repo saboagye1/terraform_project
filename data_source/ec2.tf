978037870d54a74d57d382380a04b2ac6a2420fe
Sera Aboagye <saboagye@flexera.com>
2020-07-13 12:45:56 -0500

    ec2 resource to be created

--- data_source/ec2.tf
+++ data_source/ec2.tf
@@ -42,5 +42,26 @@ data "aws_ami" "centos" {
 }
 
 output CENTOS_AMI_ID {
-  value       = "${data.aws_ami.ubuntu.id}"
+  value       = "${data.aws_ami.centos.id}"
+}
+
+
+#create an ubuntu instance using the datasource 
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+#create a centos instance using the datasource 
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.centos.id}"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
