70aa2abc1e9a71bd51ccf318ae3cdde18e02866a
Sera Aboagye <saboagye@flexera.com>
2020-07-17 12:42:33 -0500

    out put for 3-tier-app

---
+++ 3-tier-app/output.tf
@@ -0,0 +1,28 @@
+output "VPC_ID" {
+  value       = "${data.terraform_remote_state.dev.VPC_ID}"
+}
+
+output "Public_Subent1" {
+  value       = "${data.terraform_remote_state.dev.Public_Subent1}"
+}
+
+output "Public_Subent2" {
+  value       = "${data.terraform_remote_state.dev.Public_Subent2}"
+}
+
+output "Public_Subent3" {
+  value       = "${data.terraform_remote_state.dev.Public_Subnet3}"
+}
+
+
+output "Private_Subent3" {
+  value       = "${data.terraform_remote_state.dev.Private_Subnet3}"
+}
+
+output "Private_Subent3" {
+  value       = "${data.terraform_remote_state.dev.Private_Subnet3}"
+}
+
+output "Private_Subent3" {
+  value       = "${data.terraform_remote_state.dev.Private_Subnet3}"
+}
